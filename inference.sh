b308f48432409c4cd80fa55d899c8176cdc0f7ab
Ubuntu <ubuntu@sandbox-gpu-worker-sea-instance-1.asia-southeast1-c.c.emereg-beta.internal>
2024-11-12 21:03:03 +0000

    longvu docker inference

--- inference.sh
+++ inference.sh
@@ -3,4 +3,7 @@
 docker exec -it \
     $(docker ps --format '{{.Names}}' | grep longvu) \
     /opt/conda/bin/conda run -n app_env /bin/bash -c \
-    "cd app && python -u inference.py 2>&1 | tee /dev/tty"
+    "export CUDA_VISIBLE_DEVICES=0 && \
+    export PYTHONPATH=/workspace/app:$PYTHONPATH && \
+    cd app && \
+    python -u my_ext/inference.py 2>&1 | tee /dev/tty"
---
+++ longvu_pg/README.md
@@ -0,0 +1,9 @@
+# README
+
+## Setup
+
+```bash
+cd checkpoints
+git clone https://huggingface.co/Vision-CAIR/LongVU_Qwen2_7B
+mv LongVU_Qwen2_7B longvu_qwen
+```
