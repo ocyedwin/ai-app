84feeef6d866fbe255866ccee3d64ac45c595f7b
Ubuntu <ubuntu@sandbox-gpu-worker-sea-instance-1.asia-southeast1-c.c.emereg-beta.internal>
2024-11-12 20:38:48 +0000

    stream output

--- inference.sh
+++ inference.sh
@@ -3,4 +3,4 @@
 docker exec -it \
     $(docker ps --format '{{.Names}}' | grep longvu) \
     /opt/conda/bin/conda run -n app_env /bin/bash -c \
-    "cd app && python inference.py"
+    "cd app && python -u inference.py 2>&1 | tee /dev/tty"
