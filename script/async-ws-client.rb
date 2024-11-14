7674c22cc504e2390fc35411040e596d3f3040cb
edwin <edwin@ocyedwin.com>
2024-11-14 20:04:00 +0000

    multi-threaded testing

--- script/async-ws-client.rb
+++ script/async-ws-client.rb
@@ -3,28 +3,51 @@ require 'async/http/endpoint'
 require 'async/websocket/client'
 require 'json'
 
-frame = {
-  video_path: "storage/xh/za/xhzaqkh1pcsiw1z7nbhrirtqiean",
-  question: "Describe the video in detail."
-}.to_json
+# Create an array of test frames
+frames = [
+  {
+    video_path: "storage/xh/za/xhzaqkh1pcsiw1z7nbhrirtqiean",
+    question: "Describe the video in detail."
+  }.to_json,
+  {
+    video_path: "storage/m0/wb/m0wb826dhdftfcrq5qqjew9lerzg",
+    question: "What happens in this video?"
+  }.to_json
+]
 
 url = "ws://ai-app_devcontainer_longvu_pg_1:6789"
 
-Async do |task|
-  endpoint = Async::HTTP::Endpoint.parse(url)
+# Split frames into two groups
+group1, group2 = frames.each_slice((frames.length/2.0).ceil).to_a
 
-  Async::WebSocket::Client.connect(endpoint) do |connection|
-    puts "Connected to server"
-    connection.write(frame)
-    puts "Sent message to server"
+# Create two threads
+[ group1, group2 ].each_with_index do |frame_group, index|
+  Thread.new do
+    puts "Starting Thread #{index + 1}"
 
-    # Wait for and process the response
-    while message = connection.read
-      puts "Received: #{message.to_str}"
-      puts "Closing connection..."
-      connection.close
-      break
+    Async do |task|
+      frame_group.map do |frame|
+        task.async do
+          endpoint = Async::HTTP::Endpoint.parse(url)
+
+          Async::WebSocket::Client.connect(endpoint) do |connection|
+            puts "Thread #{index + 1} - Connected to server (#{frame})"
+            connection.write(frame)
+            puts "Thread #{index + 1} - Sent message to server (#{frame})"
+
+            while message = connection.read
+              puts "Thread #{index + 1} - Received for #{frame}: #{message.to_str}"
+              puts "Thread #{index + 1} - Closing connection..."
+              connection.close
+              break
+            end
+          end
+          puts "Thread #{index + 1} - Connection closed for #{frame}"
+        end
+      end.each(&:wait)
     end
   end
-  puts "Connection closed"
 end
+
+# Wait for all threads to complete
+Thread.list.each { |t| t.join unless t == Thread.current }
