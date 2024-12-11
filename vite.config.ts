d9e6107001762fc44860711f39dd4b5270b34daa
edwin <edwin@ocyedwin.com>
2024-12-11 13:10:12 +0000

    added comment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 
 export default defineConfig({
+  // NOTE: esbuild section added to support decorators for Lit
   esbuild: {
     target: "esnext",
     tsconfigRaw: {
